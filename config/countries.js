7736d40bd284cfbda554650161196b9f4bf8dc5a
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-10-25 21:22:10 +0200

    Added a config file, with the country and IP type URLs. Also updated scraper script to allow testing and accessing the URLs

---
+++ config/countries.js
@@ -0,0 +1,29 @@
+const countries = [
+  {
+    name: 'China',
+    trademarkUrl: 'https://www.ip-coster.com/IPGuides/trademark-china',
+    industrialDesignUrl: 'https://www.ip-coster.com/IPGuides/industrial-design-china',
+  },
+  {
+    name: 'USA',
+    trademarkUrl: 'https://www.ip-coster.com/IPGuides/trademark-us',
+    industrialDesignUrl: 'https://www.ip-coster.com/IPGuides/industrial-design-us',
+  },
+  {
+    name: 'Canada',
+    trademarkUrl: 'https://www.ip-coster.com/IPGuides/trademark-canada',
+    industrialDesignUrl: 'https://www.ip-coster.com/IPGuides/industrial-design-canada',
+  },
+  {
+    name: 'Switzerland',
+    trademarkUrl: 'https://www.ip-coster.com/IPGuides/trademark-switzerland',
+    industrialDesignUrl: 'https://www.ip-coster.com/IPGuides/industrial-design-switzerland',
+  },
+  {
+    name: 'United Kingdom',
+    trademarkUrl: 'https://www.ip-coster.com/IPGuides/trademark-united-kingdom',
+    industrialDesignUrl: 'https://www.ip-coster.com/IPGuides/industrial-design-united-kingdom',
+  },
+];
+
+module.exports = countries;
--- scripts/scraper.js
+++ scripts/scraper.js
@@ -1,43 +1,58 @@
 const { supabase } = require('../index');
 const puppeteer = require('puppeteer');
+const countries = require('../config/countries');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  // Navigate to the target site
-  await page.goto('https://www.ip-coster.com/IPGuides/trademark-china');
+  // Specify the country and IP type to scrape - HARD CODED FOR TESTING PURPOSES. These will come from user input in the final version.
+  const selectedCountry = 'USA'; // target country
+  const selectedIPType = 'design'; // design or trademark
 
-  // Extract Country from a specific heading using XPath
+  // Find the selected country's configuration
+  const selectedConfig = countries.find(country => country.name === selectedCountry);
+
+  if (!selectedConfig) {
+    console.log('Selected country not found in the configuration.');
+    await browser.close();
+    return;
+  }
+
+  // Navigate to the selected URL
+  await page.goto(
+    selectedIPType === 'trademark'
+      ? selectedConfig.trademarkUrl
+      : selectedConfig.industrialDesignUrl,
+  );
+
+  // Extract and log the Country
   const countryHeadingXPath =
     '/html/body/form/div[6]/div[3]/div/div[2]/div[1]/div[1]/div/section[1]/div[1]/div[1]/h1';
   const countryElement = await page.$x(countryHeadingXPath);
-
   let country = 'Country not found'; // Default value in case the heading is not found
+
   if (countryElement.length > 0) {
     country = await page.evaluate(element => element.textContent, countryElement[0]);
   }
-  console.log('Country:', country);
-  // Define the XPath expressions for the elements containing information
-  const xPaths = {
-    multipleClass: '/html/body/form/div[6]/div[3]/div/div[2]/div[1]/div[1]/div/div[3]/p[2]',
-    filingRequirements: '/html/body/form/div[6]/div[3]/div/div[2]/div[1]/div[1]/div/div[3]/p[3]',
-    examinationInfo: '/html/body/form/div[6]/div[3]/div/div[2]/div[1]/div[1]/div/div[3]/p[6]',
-    grantFee: '/html/body/form/div[6]/div[3]/div/div[2]/div[1]/div[1]/div/div[3]/p[6]',
-    validityTerm: '/html/body/form/div[6]/div[3]/div/div[2]/div[1]/div[1]/div/div[3]/p[6]',
-    useRequirement: '/html/body/form/div[6]/div[3]/div/div[2]/div[1]/div[1]/div/div[3]/p[9]',
-  };
 
-  const info = {};
-  for (const key in xPaths) {
-    const elements = await page.$x(xPaths[key]);
-    if (elements.length > 0) {
-      info[key] = await page.evaluate(element => element.textContent, elements[0]);
-      console.log(`${key}:`, info[key]);
-    } else {
-      console.log(`${key} not found.`);
-    }
+  console.log('Country:', country);
+
+  // Extract and log the Filing Requirements
+  const filingRequirementsXPath =
+    '/html/body/form/div[6]/div[3]/div/div[2]/div[1]/div[1]/div/div[3]/p[3]';
+  const filingRequirementsElement = await page.$x(filingRequirementsXPath);
+  let filingRequirements = 'Filing Requirements not found'; // Default value
+
+  if (filingRequirementsElement.length > 0) {
+    filingRequirements = await page.evaluate(
+      element => element.textContent,
+      filingRequirementsElement[0],
+    );
   }
 
+  console.log('Filing Requirements:', filingRequirements);
+
+  // Close the browser
   await browser.close();
 })();

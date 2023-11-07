c711fb5b5b8518d8d09051ce02f5901542eacbbf
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-11-07 07:53:02 +0100

    Updated testScraper.js, to test the 5 TM test countries. It works!

--- testScraper.js
+++ testScraper.js
@@ -1,38 +1,40 @@
 const puppeteer = require('puppeteer');
 const configHandler = require('./configHandler');
-const scraperLogic = require('./scraperLogic');
+const scraperLogicTrademark = require('./scraperLogicTrademark');
 
 (async () => {
   const browser = await puppeteer.launch();
 
   for (const countryData of configHandler.countries) {
-    for (const ipType of ['trademark', 'design']) {
-      const selectedConfig = configHandler.findCountryConfig(countryData.name);
+    const selectedConfig = configHandler.findCountryConfig(countryData.name);
 
-      if (!selectedConfig) {
-        console.log('Selected country not found in the configuration.');
-        continue;
-      }
+    if (!selectedConfig) {
+      console.log('Selected country not found in the configuration.');
+      continue;
+    }
 
-      const page = await browser.newPage();
-      await page.goto(
-        ipType === 'trademark' ? selectedConfig.trademarkUrl : selectedConfig.industrialDesignUrl,
+    const page = await browser.newPage();
+
+    try {
+      await page.goto(selectedConfig.trademarkUrl); // Assuming you want to scrape trademark data
+      await page.waitForTimeout(2000); // Add a delay of 2 seconds to ensure the page loads
+
+      const scrapedTrademarkData = await scraperLogicTrademark.scrapeTrademarkData(
+        page,
+        countryData.name,
+        selectedConfig,
       );
 
-      try {
-        const scrapedData = await scraperLogic.scrapeData(page);
-        if (scrapedData.country && scrapedData.filingRequirements) {
-          console.log(`Scraping succeeded for ${countryData.name} - IP Type: ${ipType}`);
-          console.log('Scraped Data:', scrapedData);
-        } else {
-          console.log(`Scraping failed for ${countryData.name} - IP Type: ${ipType}`);
-        }
-      } catch (error) {
-        console.error(`Error while scraping ${countryData.name} - IP Type: ${ipType}:`, error);
-      }
-
+      console.log(`Scraping succeeded for ${countryData.name} - IP Type: trademark`);
+      console.log('Scraped Data:', scrapedTrademarkData);
+    } catch (error) {
+      console.error(`Error while scraping ${countryData.name} - IP Type: trademark:`, error);
+    } finally {
       await page.close();
     }
+
+    // Add a delay between requests to avoid overloading the server
+    await new Promise(resolve => setTimeout(resolve, 5000)); // Delay for 5 seconds
   }
 
   await browser.close();
